9288f99c912e843737752abebc4278ecf0522799
Darnell-Chen <realdarnellchen@gmail.com>
2024-07-15 11:42:53 -0400

    need to uninstall package

---
+++ js/removeImages.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    if (iOS()) {
+        $('.program2-img').remove();
+    }
+
+    function iOS() {
+        return [
+          'iPad Simulator',
+          'iPhone Simulator',
+          'iPod Simulator',
+          'iPad',
+          'iPhone',
+          'iPod'
+        ].includes(navigator.platform)
+        // iPad on iOS 13 detection
+        || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+      }
+})
